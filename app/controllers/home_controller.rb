fda480e74227ccc52c14879fac6ddb216e2d2fa0
Yaro <yshmarov@gmail.com>
2021-06-24 10:20:55 +0000

    install devise

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  # before_action :authenticate_user!, only: [:index]
+  # before_action :authenticate_user!
+  # skip_before_action :authenticate_user!
+
+  def index
+  end
+end
